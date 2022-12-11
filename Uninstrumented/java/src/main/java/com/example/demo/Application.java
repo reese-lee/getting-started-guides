fcc0c3a9f12231994deecb579c59754817d910d5
Jack Berg <jberg@newrelic.com>
2022-12-11 12:35:32 -0600

    More idiomatic instrumentation

--- Uninstrumented/java/src/main/java/com/example/demo/Application.java
+++ Uninstrumented/java/src/main/java/com/example/demo/Application.java
@@ -2,15 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.servlet.Filter;
 
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
