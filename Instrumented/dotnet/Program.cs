65722bb4e3a8138144003532a5b31670c45f645a
Alan West <3676547+alanwest@users.noreply.github.com>
2022-12-14 14:04:44 -0800

    Remove use of obsolete stuff

--- Instrumented/dotnet/Program.cs
+++ Instrumented/dotnet/Program.cs
@@ -1,4 +1,5 @@
 using dotnet.Controllers;
+using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Metrics;
@@ -17,37 +18,37 @@ var resourceBuilder = ResourceBuilder
     .AddService("getting-started-dotnet")
     .AddTelemetrySdk();
 
-// Configure the OpenTelemetry SDK for traces
-builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
-{
-   tracerProviderBuilder
-       .SetResourceBuilder(resourceBuilder)
-       .AddSource(FibonacciController.ActivitySourceName)
-       .AddAspNetCoreInstrumentation()
-       .AddOtlpExporter((options =>
-        {
-            options.Endpoint = new Uri($"{Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT")}");
-            options.Headers = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS");
-        }));
-});
+// Configure the OpenTelemetry SDK for traces and metrics
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+            .SetResourceBuilder(resourceBuilder)
+            .AddAspNetCoreInstrumentation()
+            .AddSource(FibonacciController.ActivitySourceName)
+            .AddOtlpExporter((options =>
+            {
+                options.Endpoint = new Uri($"{Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT")}");
+                options.Headers = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS");
+            }));
+    })
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder
+            .SetResourceBuilder(resourceBuilder)
+            .AddRuntimeInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddMeter(FibonacciController.MeterName)
+            .AddOtlpExporter((exporterOptions, metricReaderOptions) =>
+            {
+                exporterOptions.Endpoint = new Uri($"{Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT")}");
+                exporterOptions.Headers = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS");
+                metricReaderOptions.TemporalityPreference = MetricReaderTemporalityPreference.Delta;
+            });
 
-// Configure the OpenTelemetry SDK for metrics
-builder.Services.AddOpenTelemetryMetrics(meterProviderBuilder =>
-{
-    meterProviderBuilder
-        .SetResourceBuilder(resourceBuilder)
-        .AddRuntimeInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddMeter(FibonacciController.MeterName)
-        .AddOtlpExporter((exporterOptions, metricReaderOptions) =>
-        {
-            exporterOptions.Endpoint = new Uri($"{Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT")}");
-            exporterOptions.Headers = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS");
-            metricReaderOptions.TemporalityPreference = MetricReaderTemporalityPreference.Delta;
-        });
-
-});
+    })
+    .StartWithHost();
 
 // Configure the OpenTelemetry SDK for logs
 builder.Logging.ClearProviders();
